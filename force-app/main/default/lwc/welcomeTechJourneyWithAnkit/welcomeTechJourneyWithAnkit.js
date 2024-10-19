38a7a4113e0b0e1fd906ababd2e422abc8dbbd1a
Mrinmoy <mrinmoyd629@gmail.com>
2024-10-19 23:07:52 +0600

    dcorator practice

--- force-app/main/default/lwc/welcomeTechJourneyWithAnkit/welcomeTechJourneyWithAnkit.js
+++ force-app/main/default/lwc/welcomeTechJourneyWithAnkit/welcomeTechJourneyWithAnkit.js
@@ -1,3 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class WelcomeTechJourneyWithAnkit extends LightningElement {}
+export default class WelcomeTechJourneyWithAnkit extends LightningElement {
+    greeting = 'Hello';
+    @track welcome = 'Tech Journey With Ankit';
+
+    clickHandler(event){
+         this.greeting = 'Namaste';
+         this.welcome = 'Today is day 19 of LWC Bootcamp';
+    }
+}
